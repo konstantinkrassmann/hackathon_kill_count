059308970bfa758a3c9d7925fd79a6a6b15434ae
Kostja <kkonstantin@gmx.de>
2014-11-01 18:13:36 +0100

    Improve charts

--- app/core/directives/_verticalBarChart.js
+++ app/core/directives/_verticalBarChart.js
@@ -4,159 +4,152 @@
 hackAppKillCount.directive("verticalBarChart", function () {
     return {
         scope: {
-            "dataSet": "=verticalBarChart"
+            "dataSet": "=verticalBarChart",
+            "tipTemplate": "="
         },
         link: function (scope, element) {
             //Width and height
-            var diagramWidth = 600,
-                diagramHeight = 300,
-                barWidth = 20,
-                barPadding = 2,
-                svgPaddingLeft = 25,
-                svgPaddingTop = 0;
-
-
+            var margin = {top: 20, right: 20, bottom: 70, left: 40},
+                width = 600 - margin.left - margin.right,
+                height = 500 - margin.top - margin.bottom;
 
             var svg = d3.select("body")
                 .append("svg")
-                .attr("width", diagramWidth)
-                .attr("height", diagramHeight)
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
                 .append("g")
-                .attr("transform", "translate("+svgPaddingLeft+", "+svgPaddingTop+")")
-
+                .attr("transform",
+                    "translate(" + margin.left + "," + margin.top + ")");
 
 
             scope.render = function () {
+
+                width = $(".chart-box").outerWidth(true) - margin.left - margin.right;
+                svg.attr("width", width + margin.left + margin.right)
+                    .attr("height", height + margin.top + margin.bottom);
+
                 //calculate max for the scale
-                var victimsMax = d3.max(scope.dataSet, function (d) {
+                var victimsHighestMax = d3.max(scope.dataSet, function (d) {
+                    return +d.highestCasualties;
+                });
+
+                //calculate max for the scale
+                var victimsLowestMax = d3.max(scope.dataSet, function (d) {
                     return +d.lowestCasualties;
                 });
 
+
+
+                var victimsMax = victimsHighestMax > victimsLowestMax ?  victimsHighestMax: victimsLowestMax;
+
+                console.log(victimsMax)
+
+                function compare(a,b){
+                    if(a.lowestCasualties > b.lowestCasualties)
+                    {
+                        return 1;
+                    }
+                    if(a.lowestCasualties > b.lowestCasualties){
+                        return -1;
+                    }
+                    return 0;
+                }
+
+              //  console.log(scope.dataSet.sort(compare));
+
+
                 //create scale for height
                 var linearScale = d3.scale.linear()
-                    .domain([0, victimsMax])
-                    .range([0, diagramHeight]);
+                    .domain([0, victimsMax + (victimsMax*0.1)])
+                    .range([0, height]);
 
                 var bloodColorScale = d3.scale.linear()
                     .domain([0, victimsMax])
                     .range(["#FF0000", "#990000"]);
 
-                var axis = d3.svg.axis()
+                var yAxis = d3.svg.axis()
                     .scale(linearScale)
-                    .orient("left")
-                var mainGroup = svg.append("g")
-                    .attr("transform", "translate(10,10)");
+                    .orient("left");
 
+                var tip = d3.tip()
+                    .attr('class', 'd3-tip')
+                    .offset([-10, 0])
+                    .html(scope.tipTemplate);
 
-                var axisGroup =  mainGroup.append("g")
-                    .attr("class", "y axis")
-                    .call(axis)
-/*
-                var rects = svg.selectAll("rect")
+                svg.append("g")
+                    .attr("class", "x axis")
+                    .attr("transform", "translate("+margin.left+",0)")
+                    .call(yAxis);
+
+                svg.call(tip);
+
+                svg.selectAll("bar")
+                    .append("g")
                     .data(scope.dataSet)
                     .enter()
                     .append("rect")
-                    .attr("x", function (d, i) {
-                        return (i * (diagramWidth / scope.dataSet.length)) + barPadding;
+                    .attr("x", function(d,i){
+                        return i* (width / scope.dataSet.length + 1) + margin.left + 5;
+                    })
+                    .attr("width", function(){
+                        return width / scope.dataSet.length;
                     })
                     .attr("y", 0)
-                    .attr("width", (diagramWidth / scope.dataSet.length) - barPadding)
                     .attr("height", 0)
-                    .attr("fill", function (d) {
-                        return "rgb(" + (d.lowestCasualties * 10) + ", 0, 0)";
+                    .attr("fill", function(d){
+                        return bloodColorScale(d.highestCasualties);
+                    })
+                   // .on('mouseover', tip.show)
+                   // .on('mouseout', tip.hide)
+                    .on("click", function(d){
+                        d.blTipVisible = !d.blTipVisible;
+                        if(d.blTipVisible)
+                        {
+                            tip.show(d);
+                        }else{
+                            tip.hide(d);
+                        }
                     })
                     .transition()
                     .duration(2000)
-                    .attr("height", function (d) {
+                    .attr("height", function(d){
+                        return linearScale(d.highestCasualties);
+                    });
+
+
+                svg.selectAll("bar")
+                    .append("g")
+                    .data(scope.dataSet)
+                    .enter()
+                    .append("rect")
+                    .attr("x", function(d,i){
+                        return i* (width / scope.dataSet.length + 1) + margin.left + 5;
+                    })
+                    .attr("width", function(){
+                        return width / scope.dataSet.length;
+                    })
+                    .attr("y", 0)
+                    .attr("height", 0)
+                    .attr("fill", function(d){
+                        return bloodColorScale(d.lowestCasualties);
+                    })
+                    // .on('mouseover', tip.show)
+                    // .on('mouseout', tip.hide)
+                    .on("click", function(d){
+                        d.blTipVisible = !d.blTipVisible;
+                        if(d.blTipVisible)
+                        {
+                            tip.show(d);
+                        }else{
+                            tip.hide(d);
+                        }
+                    })
+                    .transition()
+                    .duration(2000)
+                    .attr("height", function(d){
                         return linearScale(d.lowestCasualties);
-                    })
-                    .attr("fill", function (d) {
-                        return bloodColorScale(d.lowestCasualties);
-                    });
-*/
-                // Returns path data for a rectangle with rounded right corners.
-// The top-left corner is ?x,y?.
-                function rounded_rect(x, y, w, h, r, tl, tr, bl, br) {
-                    var retval;
-                    retval  = "M" + (x + r) + "," + y;
-                    retval += "h" + (w - 2*r);
-                    if (tr) { retval += "a" + r + "," + r + " 0 0 1 " + r + "," + r; }
-                    else { retval += "h" + r; retval += "v" + r; }
-                    retval += "v" + (h - 2*r);
-                    if (br) { retval += "a" + r + "," + r + " 0 0 1 " + -r + "," + r; }
-                    else { retval += "v" + r; retval += "h" + -r; }
-                    retval += "h" + (2*r - w);
-                    if (bl) { retval += "a" + r + "," + r + " 0 0 1 " + -r + "," + -r; }
-                    else { retval += "h" + -r; retval += "v" + -r; }
-                    retval += "v" + (2*r - h);
-                    if (tl) { retval += "a" + r + "," + r + " 0 0 1 " + r + "," + -r; }
-                    else { retval += "v" + -r; retval += "h" + r; }
-                    retval += "z";
-                    return retval;
-                }
-
-                mainGroup.selectAll("path")
-                    .data(scope.dataSet)
-                    .enter()
-                    .append("path")
-                    .attr("fill", "red")
-                    .attr("d", function (d, i) {
-                        return rounded_rect(
-                            (i * (diagramWidth / scope.dataSet.length)) + barPadding ,
-                            0,
-                            (diagramWidth / scope.dataSet.length) - barPadding,
-                            0,
-                            8,
-                            false,
-                            false,
-                            true,
-                            true
-                        )
-                    })
-                    .transition()
-                    .duration(2000)
-                    .attr("d", function (d, i) {
-                        return rounded_rect(
-                            (i * (diagramWidth / scope.dataSet.length)) + barPadding,
-                            0,
-                            (diagramWidth / scope.dataSet.length) - barPadding,
-                            linearScale(d.lowestCasualties),
-                            8,
-                            false,
-                            false,
-                            true,
-                            true
-                        )
-                    })
-                    .attr("fill", function (d) {
-                        return bloodColorScale(d.lowestCasualties);
                     });
 
-                svg.selectAll("text")
-                    .data(scope.dataSet)
-                    .enter()
-                    .append("text")
-                    .text(function (d) {
-                        return d.lowestCasualties;
-                    })
-                    .attr("x", function (d, i) {
-                        return i * (diagramWidth / scope.dataSet.length) - 10;  // +5
-                    })
-                    .attr("y", function (d) {
-                        return linearScale(d.lowestCasualties);              // +15
-                    })
-                    .attr("text-anchor", "middle")
-                    .attr("rotate", 90)
-                    .attr("font-family", "sans-serif")
-                    .attr("font-size", "11px")
-                    .attr("fill", "white")
-                //    .attr("text-anchor", "middle");
-                     //.attr("transform", "rotate(90)");
-
-                /*.delay(1000)
-                 .attr("x", function(d, i){
-                 return i* (barWidth+barPadding);
-                 })*/
             };
 
             scope.$watch("dataSet", function () {
@@ -165,3 +158,87 @@ hackAppKillCount.directive("verticalBarChart", function () {
         }
     };
 });
+
+
+/*
+
+
+ var test = victimsMax + "";
+
+ var mainGroupMarginTop= 10;
+ var mainGroupMarginLeft = 10 + (test.length*5);
+
+
+ var mainGroup = svg.append("g")
+ .attr("transform", "translate("+mainGroupMarginLeft+","+mainGroupMarginTop+")");
+
+ var axisGroup =  svg.append("g")
+ .attr("class", "y axis")
+ .attr("transform", "translate("+mainGroupMarginLeft+","+mainGroupMarginTop+")")
+ //.call(axis);
+
+
+ // Returns path data for a rectangle with rounded right corners.
+ // The top-left corner is ?x,y?.
+ function rounded_rect(x, y, w, h, r, tl, tr, bl, br) {
+ var retval;
+ retval  = "M" + (x + r) + "," + y;
+ retval += "h" + (w - 2*r);
+ if (tr) { retval += "a" + r + "," + r + " 0 0 1 " + r + "," + r; }
+ else { retval += "h" + r; retval += "v" + r; }
+ retval += "v" + (h - 2*r);
+ if (br) { retval += "a" + r + "," + r + " 0 0 1 " + -r + "," + r; }
+ else { retval += "v" + r; retval += "h" + -r; }
+ var _val = 2*r - w;
+ retval += "h" + (_val < 0 ? 0: _val);
+ if (bl) { retval += "a" + r + "," + r + " 0 0 1 " + -r + "," + -r; }
+ else { retval += "h" + -r; retval += "v" + -r; }
+ retval += "v" + (_val < 0 ? 0: _val);
+ if (tl) { retval += "a" + r + "," + r + " 0 0 1 " + r + "," + -r; }
+ else { retval += "v" + -r; retval += "h" + r; }
+ retval += "z";
+ return retval;
+ }
+
+ svg.selectAll("path")
+ .data(scope.dataSet)
+ .enter()
+ .append("path")
+ .attr("fill", "red")
+ .attr("d", function (d, i) {
+ return rounded_rect(
+ (i * (diagramWidth / scope.dataSet.length)) + barPadding ,
+ mainGroupMarginTop,
+ (diagramWidth / scope.dataSet.length) - barPadding,
+ 0,
+ 8,
+ false,
+ false,
+ true,
+ true
+ )
+ })
+ .transition()
+ .duration(2000)
+ .attr("d", function (d, i) {
+ var barHeight = linearScale(d.lowestCasualties);
+ if(barHeight < 2){
+ barHeight+= 2;
+ }
+
+ return rounded_rect(
+ (i * (diagramWidth / scope.dataSet.length)) + barPadding,
+ mainGroupMarginTop,
+ (diagramWidth / scope.dataSet.length) - barPadding,
+ barHeight,
+ 8,
+ false,
+ false,
+ true,
+ true
+ )
+ })
+ .attr("fill", function (d) {
+ return bloodColorScale(d.lowestCasualties);
+ });
+ */
--- app/views/start/StartController.js
+++ app/views/start/StartController.js
@@ -2,30 +2,65 @@
  * Created by Kostja on 31.10.14.
  */
 hackAppKillCount.controller("StartController", function($scope, DAO){
-    $scope.START ="StartController";
 
-    $scope.categories = [
-        'Witch_Hunt',
-        'Serial_killer'
-    ];
+    $scope.selectedCategory ="";
+    $scope.CATEGORY =  {
+        "SERIAL_KILLER": "sk",
+        "GENOCIDE": "g",
+        "TERRORISM":"t"
+    };
 
+    $scope.blLoading = true;
 
+    $scope.selectCategory = function (strCategory) {
+        switch (strCategory){
+            case $scope.CATEGORY.SERIAL_KILLER:
+                if($scope.serial_killers.length == 0){
+                    DAO.getSerialKillers().then(function(killers){
+                        console.log(arguments);
+                        $scope.blLoading = false;
+                        $scope.serial_killers = killers;
+                    });
+                }
+            break;
+            case $scope.CATEGORY.GENOCIDE:
+                if($scope.genocides.length == 0){
+                    $scope.blLoading = true;
+                    DAO.getGenocides().then(function(genocides){
+                        console.log(genocides);
+                        $scope.genocides = genocides;
+                        $scope.blLoading = false;
+                    });
+                }
+                break;
+        }
+        $scope.selectedCategory = strCategory;
+    };
 
     $scope.serial_killers = [];
+    $scope.serial_killer_tip_template =
+        function(d) {
+            return '<strong>Name:</strong> <span class="text-danger">' + d.name + '</span> <br>' +
+                '<strong>Bewiesene Morde:</strong> <span class="text-danger">' + d.lowestCasualties + '</span> <br>' +
+                '<strong>Vermutete Morde:</strong> <span class="text-danger">' + d.highestCasualties + '</span> <br>' +
+                '<strong>Aktivität:</strong> <span class="text-danger">' + d.yearsActive + '</span> <br>' +
+                '<strong>Region:</strong> <span class="text-danger">' + d.region + '</span> <br>' +
+                '<a class="text-primary" href="'+ d.wikipediaLink +'" class="text-danger" target="_blank">Mehr erfahren</a> <br>'
+        };
 
-    $scope.blLoadingKillers = true;
 
-    DAO.getSerialKillers().then(function(killers){
-        console.log(arguments);
-        $scope.blLoadingKillers = false;
-        $scope.serial_killers = killers;
-    });
 
     $scope.genocides = [];
+    $scope.genocide_tip_template =
+        function(d) {
+            return '<strong>Name:</strong> <span class="text-danger">' + d.event + '</span> <br>' +
+                '<strong>Bewiesene Morde:</strong> <span class="text-danger">' + d.lowestCasualties + '</span> <br>' +
+                '<strong>Vermutete Morde:</strong> <span class="text-danger">' + d.highestCasualties + '</span> <br>' +
+                '<strong>Zeitraum:</strong> <span class="text-danger">' + d.from + ' bis '+ d.to+'</span> <br>' +
+                '<strong>Region:</strong> <span class="text-danger">' + d.region + '</span> <br>' +
+                '<a class="text-primary" href="'+ d.wikipediaLink +'" class="text-danger" target="_blank">Mehr erfahren</a> <br>'
+        };
 
-    $scope.blLoadingGenocides = true;
-    DAO.getGenocides().then(function(genocides){
-        $scope.genocides = genocides;
-        $scope.blLoadingGenocides = false;
-    });
+
+
 });
